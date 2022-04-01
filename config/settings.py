8d8b43b46b9b6edcaa7f77c794d264ee3eea17b6
ShuhratXayitov <65951559+ShuhratX@users.noreply.github.com>
2022-04-01 17:39:50 +0500

    cors allowed origins

--- config/settings.py
+++ config/settings.py
@@ -136,9 +136,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    ".herokuapp.com",
-    "http://localhost:3000/", 
-    "http://localhost:8080",
-    "http://127.0.0.1:8000"
-]
+CORS_ALLOWED_ALL_ORIGINS = True
