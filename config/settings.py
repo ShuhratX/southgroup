451c7e6b1bbd62070472368b5f89d87165f29387
ShuhratXayitov <65951559+ShuhratX@users.noreply.github.com>
2022-04-07 14:35:05 +0500

    cors headers

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".herokuapp.com", "127.0.0.1", "http://localhost:3000/", "https://southgroup.netlify.app"]
+ALLOWED_HOSTS = [".herokuapp.com", "127.0.0.1", "http://localhost:3000/", ".netlify.app"]
 
 
 # Application definition
